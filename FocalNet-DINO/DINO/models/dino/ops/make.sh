a8bcd4093132089cd48776257bc79da015995ece
ameya135 <ameyapatil103@gmail.com>
2025-04-17 13:44:52 +0530

    added focalnet-dino

---
+++ FocalNet-DINO/DINO/models/dino/ops/make.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------------------------
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------------------------------
+# Modified from https://github.com/chengdazhi/Deformable-Convolution-V2-PyTorch/tree/pytorch_1.0.0
+# ------------------------------------------------------------------------------------------------
+
+python setup.py build install
+
+# Following command can be used to build for a specific architecture
+# TORCH_CUDA_ARCH_LIST="8.0" ROCM_HOME='/path/to/your/rocm/dir'  
+python setup.py build install
